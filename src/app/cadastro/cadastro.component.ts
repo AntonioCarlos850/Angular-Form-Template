9720f902c0c715ba8af49c63d500ada990991c12
AntonioCarlos850 <antoniocarlosjr850@gmail.com>
2023-10-15 20:26:08 -0300

    feature/cadastro: validate form

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,5 +1,7 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { CepService } from '../services/cep.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -8,12 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private service: CepService) { }
 
   ngOnInit(): void {
   }
 
-  cadastrar(){
-      console.log('Formulário enviado');
+  validateCep(e: any, form: NgForm){
+    const cep = e.target.value;
+    if (cep) {
+      this.service.getCep(cep).subscribe(
+        data => this.fillAddress(data, form)
+      );
+    }
+  }
+
+  fillAddress(data: any, form: NgForm){
+    form.form.patchValue({
+      endereco: data.logradouro,
+      complemento: data.complemento,
+      bairro: data.bairro,
+      cidade: data.localidade,
+      estado: data.uf
+    });
+  }
+
+  register(form: NgForm){
+      if (form.valid) {
+        this.router.navigate(['/sucesso']);
+      }
   }
 }
